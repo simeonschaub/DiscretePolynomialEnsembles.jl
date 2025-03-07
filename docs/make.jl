7bb11ed6c9a049733de4d203de073d43cdb6fae4
Simeon David Schaub <simeon@schaub.rocks>
2025-03-07 11:56:47 +0100

    fix formatting

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using PolynomialEnsembles
 using Documenter
 
-DocMeta.setdocmeta!(PolynomialEnsembles, :DocTestSetup, :(using PolynomialEnsembles); recursive=true)
+DocMeta.setdocmeta!(PolynomialEnsembles, :DocTestSetup, :(using PolynomialEnsembles); recursive = true)
 
 makedocs(;
-    modules=[PolynomialEnsembles],
-    authors="Simeon David Schaub <simeon@schaub.rocks> and contributors",
-    sitename="PolynomialEnsembles.jl",
-    format=Documenter.HTML(;
-        canonical="https://simeonschaub.github.io/PolynomialEnsembles.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [PolynomialEnsembles],
+    authors = "Simeon David Schaub <simeon@schaub.rocks> and contributors",
+    sitename = "PolynomialEnsembles.jl",
+    format = Documenter.HTML(;
+        canonical = "https://simeonschaub.github.io/PolynomialEnsembles.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/simeonschaub/PolynomialEnsembles.jl",
-    devbranch="main",
+    repo = "github.com/simeonschaub/PolynomialEnsembles.jl",
+    devbranch = "main",
 )
