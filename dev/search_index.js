b3d0fd28d28ba83489a3dae689567cfd1006f7df
Documenter.jl <documenter@juliadocs.github.io>
2025-03-07 10:56:32 +0000

    build based on d8215bb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PolynomialEnsembles","category":"page"},{"location":"#PolynomialEnsembles","page":"Home","title":"PolynomialEnsembles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PolynomialEnsembles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PolynomialEnsembles]","category":"page"}]
+}
