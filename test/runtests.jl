78c687a0128e55b8752261861ef4e93d4b321887
Simeon David Schaub <simeon@schaub.rocks>
2025-03-12 10:25:30 +0100

    fix Christoffel-Darboux tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,16 +26,18 @@ end
 @testitem "Christoffel-Darboux" begin
     using LinearAlgebra
 
-    @testset "$ensemble" for ensemble in [
-            Meixner(; K = 7, q = 0.6), Krawtchouk(; K = 30, p = 0.3), Charlier(; a = 0.5),
-        ]
+    @testset "$ensemble" for (ensemble, rtol) in zip(
+            [Meixner(; K = 7, q = 0.6), Krawtchouk(; K = 30, p = 0.3), Charlier(; a = 0.5)],
+            [1.0e-10, 1.0e-13, 1.0e-15],
+        )
         A = Kernel(ensemble, big(10)).(0:10, (0:10)')
         B = broadcast(0:10, (0:10)') do x, y
             sum(0:9) do j
                 fⱼ = ensemble[big(j)]
                 fⱼ(x) * fⱼ(y) / LinearAlgebra.norm_sqr(fⱼ)
-            end
+            end  * √(weight(ensemble, x) * weight(ensemble, y))
         end
-        @test_broken A ≈ B
+
+        @test A ≈ B rtol = rtol
     end
 end
