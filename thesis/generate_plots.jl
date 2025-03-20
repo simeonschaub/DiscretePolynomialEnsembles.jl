c708a62a39b931cc481dcfb406583c1787d50568
Simeon David Schaub <simeon@schaub.rocks>
2025-03-20 17:23:11 +0100

    wip plots

--- thesis/generate_plots.jl
+++ thesis/generate_plots.jl
@@ -11,12 +11,12 @@ end
 
 fig = joinpath(@__DIR__, "fig")
 
-save("$fig/meixner.pdf", plot_polynomials(Meixner(; K = 1, q = 0.5), 0:5, 0..5))
-save("$fig/krawtchouk.pdf", plot_polynomials(Krawtchouk(; K = 5, p = 0.5), 0:5, 0..5))
-save("$fig/charlier.pdf", plot_polynomials(Charlier(; a = 2.0), 0:5, 0..5))
-save("$fig/discrete_legendre.pdf", plot_polynomials(DiscreteLegendre(; N = 5), 0:5, 0..5))
+save("$fig/meixner.svg", plot_polynomials(Meixner(; K = 1, q = 0.5), 0:5, 0..5))
+save("$fig/krawtchouk.svg", plot_polynomials(Krawtchouk(; K = 5, p = 0.5), 0:5, 0..5))
+save("$fig/charlier.svg", plot_polynomials(Charlier(; a = 2.0), 0:5, 0..5))
+save("$fig/discrete_legendre.svg", plot_polynomials(DiscreteLegendre(; N = 5), 0:5, 0..5))
 
-using OhMyThreads, GenericLinearAlgebra, Distributions
+using OhMyThreads, GenericLinearAlgebra, Distributions, Statistics
 using Distributions: Categorical
 
 function prepare_dpp(ensemble, N, cutoff)
@@ -36,4 +36,31 @@ function randDPPproj(Y)
 		Y = (Y * qr(Y[𝓘[k], :]).Q)[:, 2:end]
 	end
 	return sort(𝓘)
+end
+
+function prepare_hist(f!, N, groups, iters = 10000; prepare = Returns(nothing), binedges = -0.5:40.5)
+	hists = [Hist1D(; counttype = Int, binedges) for _ in 1:N, _ in 1:groups]
+	@tasks for _ in 1:iters
+		tmp = prepare()
+		for i in 1:groups
+			λ = f!(tmp)
+			atomic_push!.(@view(hists1[:, i]), λ)
+		end
+	end
+	hists_mean = map(1:N) do i
+		c = stack(bincounts.(@view(hists1[i, :])))
+		m = mean(c; dims = 2)
+		Hist1D(; binedges, bincounts = vec(m))
+	end
+	hists_errors = map(1:N) do i
+		c = stack(bincounts.(normalize.(@view(hists1[i, :]))))
+		m = mean(c; dims = 2)
+		s = std(c; dims = 2)
+		Vec3f.(bincenters(hists[1]), vec(m), vec(s))
+	end
+	return hists_mean, hists_errors
+end
+
+function dpp_hist(Y, N, groups, iters = 10000)
+	return prepare_hist(_ -> randDPPproj(Y), N, groups, iters)
 end
