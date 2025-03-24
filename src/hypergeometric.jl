a652067cf32e04bd151fb03c1d4ee07f74e51adb
Simeon David Schaub <simeon@schaub.rocks>
2025-03-24 16:58:25 +0100

    licensing

--- src/hypergeometric.jl
+++ src/hypergeometric.jl
@@ -1,168 +1,20 @@
-### adapted from:
-#template <bool calc_a1, bool calc_a2, bool calc_b1, typename T1, typename T2,
-#          typename T3, typename T_z,
-#          typename ScalarT = return_type_t<T1, T2, T3, T_z>,
-#          typename TupleT = std::tuple<ScalarT, ScalarT, ScalarT>>
-#TupleT grad_2F1_impl_ab(const T1& a1, const T2& a2, const T3& b1, const T_z& z,
-#                        double precision = 1e-14, int max_steps = 1e6) {
-#  TupleT grad_tuple = TupleT(0, 0, 0);
-#
-#  if (z == 0) {
-#    return grad_tuple;
-#  }
-#
-#  using ScalarArrayT = Eigen::Array<ScalarT, 3, 1>;
-#  ScalarArrayT log_g_old = ScalarArrayT::Constant(3, 1, NEGATIVE_INFTY);
-#
-#  ScalarT log_t_old = 0.0;
-#  ScalarT log_t_new = 0.0;
-#  int sign_z = sign(z);
-#  auto log_z = log(abs(z));
-#
-#  int log_t_new_sign = 1.0;
-#  int log_t_old_sign = 1.0;
-#
-#  Eigen::Array<int, 3, 1> log_g_old_sign = Eigen::Array<int, 3, 1>::Ones(3);
-#
-#  int sign_zk = sign_z;
-#  int k = 0;
-#  const int min_steps = 5;
-#  ScalarT inner_diff = 1;
-#  ScalarArrayT g_current = ScalarArrayT::Zero(3);
-#
-#  while ((inner_diff > precision || k < min_steps) && k < max_steps) {
-#    ScalarT p = ((a1 + k) * (a2 + k) / ((b1 + k) * (1.0 + k)));
-#    if (p == 0) {
-#      return grad_tuple;
-#    }
-#    log_t_new += log(fabs(p)) + log_z;
-#    log_t_new_sign = sign(value_of_rec(p)) * log_t_new_sign;
-#
-#    if (calc_a1) {
-#      ScalarT term_a1
-#          = log_g_old_sign(0) * log_t_old_sign * exp(log_g_old(0) - log_t_old)
-#            + inv(a1 + k);
-#      log_g_old(0) = log_t_new + log(abs(term_a1));
-#      log_g_old_sign(0) = sign(value_of_rec(term_a1)) * log_t_new_sign;
-#      g_current(0) = log_g_old_sign(0) * exp(log_g_old(0)) * sign_zk;
-#      std::get<0>(grad_tuple) += g_current(0);
-#    }
-#
-#    if (calc_a2) {
-#      ScalarT term_a2
-#          = log_g_old_sign(1) * log_t_old_sign * exp(log_g_old(1) - log_t_old)
-#            + inv(a2 + k);
-#      log_g_old(1) = log_t_new + log(abs(term_a2));
-#      log_g_old_sign(1) = sign(value_of_rec(term_a2)) * log_t_new_sign;
-#      g_current(1) = log_g_old_sign(1) * exp(log_g_old(1)) * sign_zk;
-#      std::get<1>(grad_tuple) += g_current(1);
-#    }
-#
-#    if (calc_b1) {
-#      ScalarT term_b1
-#          = log_g_old_sign(2) * log_t_old_sign * exp(log_g_old(2) - log_t_old)
-#            + inv(-(b1 + k));
-#      log_g_old(2) = log_t_new + log(abs(term_b1));
-#      log_g_old_sign(2) = sign(value_of_rec(term_b1)) * log_t_new_sign;
-#      g_current(2) = log_g_old_sign(2) * exp(log_g_old(2)) * sign_zk;
-#      std::get<2>(grad_tuple) += g_current(2);
-#    }
-#
-#    inner_diff = g_current.array().abs().maxCoeff();
-#
-#    log_t_old = log_t_new;
-#    log_t_old_sign = log_t_new_sign;
-#    sign_zk *= sign_z;
-#    ++k;
-#  }
-#
-#  if (k > max_steps) {
-#    throw_domain_error("grad_2F1", "k (internal counter)", max_steps,
-#                       "exceeded ",
-#                       " iterations, hypergeometric function gradient "
-#                       "did not converge.");
-#  }
-#  return grad_tuple;
-#}
-#
-#template <bool calc_a1, bool calc_a2, bool calc_b1, bool calc_z, typename T1,
-#          typename T2, typename T3, typename T_z,
-#          typename ScalarT = return_type_t<T1, T2, T3, T_z>,
-#          typename TupleT = std::tuple<ScalarT, ScalarT, ScalarT, ScalarT>>
-#TupleT grad_2F1_impl(const T1& a1, const T2& a2, const T3& b1, const T_z& z,
-#                     double precision = 1e-14, int max_steps = 1e6) {
-#  bool euler_transform = false;
-#  try {
-#    check_2F1_converges("hypergeometric_2F1", a1, a2, b1, z);
-#  } catch (const std::exception& e) {
-#    // Apply Euler's hypergeometric transformation if function
-#    // will not converge with current arguments
-#    check_2F1_converges("hypergeometric_2F1 (euler transform)", b1 - a1, a2, b1,
-#                        z / (z - 1));
-#    euler_transform = true;
-#  }
-#
-#  std::tuple<ScalarT, ScalarT, ScalarT> grad_tuple_ab;
-#  TupleT grad_tuple_rtn = TupleT(0, 0, 0, 0);
-#  if (euler_transform) {
-#    ScalarT a1_euler = a2;
-#    ScalarT a2_euler = b1 - a1;
-#    ScalarT z_euler = z / (z - 1);
-#    if (calc_z) {
-#      auto hyper1 = hypergeometric_2F1(a1_euler, a2_euler, b1, z_euler);
-#      auto hyper2 = hypergeometric_2F1(1 + a2, 1 - a1 + b1, 1 + b1, z_euler);
-#      std::get<3>(grad_tuple_rtn)
-#          = a2 * pow(1 - z, -1 - a2) * hyper1
-#            + (a2 * (b1 - a1) * pow(1 - z, -a2)
-#               * (inv(z - 1) - z / square(z - 1)) * hyper2)
-#                  / b1;
-#    }
-#    if (calc_a1 || calc_a2 || calc_b1) {
-#      // 'a' gradients under Euler transform are constructed using the gradients
-#      // of both elements, so need to compute both if any are required
-#      constexpr bool calc_a1_euler = calc_a1 || calc_a2;
-#      // 'b' gradients under Euler transform require gradients from 'a2'
-#      constexpr bool calc_a2_euler = calc_a1 || calc_a2 || calc_b1;
-#      grad_tuple_ab = grad_2F1_impl_ab<calc_a1_euler, calc_a2_euler, calc_b1>(
-#          a1_euler, a2_euler, b1, z_euler);
-#
-#      auto pre_mult_ab = inv(pow(1.0 - z, a2));
-#      if (calc_a1) {
-#        std::get<0>(grad_tuple_rtn) = -pre_mult_ab * std::get<1>(grad_tuple_ab);
-#      }
-#      if (calc_a2) {
-#        auto hyper_da2 = hypergeometric_2F1(a1_euler, a2, b1, z_euler);
-#        std::get<1>(grad_tuple_rtn)
-#            = -pre_mult_ab * hyper_da2 * log1m(z)
-#              + pre_mult_ab * std::get<0>(grad_tuple_ab);
-#      }
-#      if (calc_b1) {
-#        std::get<2>(grad_tuple_rtn)
-#            = pre_mult_ab
-#              * (std::get<1>(grad_tuple_ab) + std::get<2>(grad_tuple_ab));
-#      }
-#    }
-#  } else {
-#    if (calc_z) {
-#      auto hyper_2f1_dz = hypergeometric_2F1(a1 + 1.0, a2 + 1.0, b1 + 1.0, z);
-#      std::get<3>(grad_tuple_rtn) = (a1 * a2 * hyper_2f1_dz) / b1;
-#    }
-#    if (calc_a1 || calc_a2 || calc_b1) {
-#      grad_tuple_ab
-#          = grad_2F1_impl_ab<calc_a1, calc_a2, calc_b1>(a1, a2, b1, z);
-#      if (calc_a1) {
-#        std::get<0>(grad_tuple_rtn) = std::get<0>(grad_tuple_ab);
-#      }
-#      if (calc_a2) {
-#        std::get<1>(grad_tuple_rtn) = std::get<1>(grad_tuple_ab);
-#      }
-#      if (calc_b1) {
-#        std::get<2>(grad_tuple_rtn) = std::get<2>(grad_tuple_ab);
-#      }
-#    }
-#  }
-#  return grad_tuple_rtn;
-#}
+# This code is adapted from the Stan Math Library, which is licensed under the BSD 3-Clause License.
+# Original code can be found here: https://mc-stan.org/math/grad__2_f1_8hpp_source.html
+
+### BSD 3-Clause License
+###
+### Copyright (c) 2011-2020, Stan Developers and their Assignees
+### All rights reserved.
+###
+### Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+###
+### * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+###
+### * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+###
+### * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+###
+### THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using Arblib: ArbLike
 
