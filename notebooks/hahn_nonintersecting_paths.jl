42c5ec11d109b47bbafbe16bfa70a4f822f1af02
Simeon David Schaub <simeon@schaub.rocks>
2025-04-18 14:15:47 +0200

    wip

--- notebooks/hahn_nonintersecting_paths.jl
+++ notebooks/hahn_nonintersecting_paths.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.4
+# v0.20.6
 
 using Markdown
 using InteractiveUtils
@@ -857,7 +857,6 @@ NetworkLayout = "~0.4.10"
 OhMyThreads = "~0.8.2"
 PairPlots = "~3.0.1"
 Revise = "~3.7.3"
-Statistics = "~1.11.1"
 WGLMakie = "~0.11.3"
 """
 
@@ -865,9 +864,9 @@ WGLMakie = "~0.11.3"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.11.4"
+julia_version = "1.11.5"
 manifest_format = "2.0"
-project_hash = "7caa60ee9ccdde00a2114f7642a4de0261f0e4c4"
+project_hash = "85917a73b053b597f71ed19383883b7000aacb2d"
 
 [[deps.AbstractFFTs]]
 deps = ["LinearAlgebra"]
@@ -2024,7 +2023,7 @@ version = "3.2.4+0"
 [[deps.OpenLibm_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "05823500-19ac-5b8b-9628-191a04bc5112"
-version = "0.8.1+4"
+version = "0.8.5+0"
 
 [[deps.OpenSSL]]
 deps = ["BitFlags", "Dates", "MozillaCACerts_jll", "OpenSSL_jll", "Sockets"]
