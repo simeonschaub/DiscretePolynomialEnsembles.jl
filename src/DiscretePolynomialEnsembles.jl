3bb8b0ee1affa620e1f59dfbd83d33ae7d88529c
Simeon David Schaub <simeon@schaub.rocks>
2026-01-27 14:14:39 +0100

    Lanczos + notebooks (#17)

--- src/DiscretePolynomialEnsembles.jl
+++ src/DiscretePolynomialEnsembles.jl
@@ -9,7 +9,8 @@ using Arblib
 MaybeDualArb = Union{Arb, Dual{<:Any, Arb}}
 
 export DiscretePolynomialEnsemble, weight, Kernel,
-    Meixner, Krawtchouk, Charlier, DiscreteLegendre, Hahn, BesselJ
+    Meixner, Krawtchouk, Charlier, DiscreteLegendre, Hahn, BesselJ,
+    Lanczos, LanczosMonic
 
 abstract type PolynomialEnsemble end
 abstract type DiscretePolynomialEnsemble <: PolynomialEnsemble end
@@ -31,7 +32,7 @@ function ((; ensemble, n)::BasisElement{true})(x)
 end
 LinearAlgebra.norm_sqr(::BasisElement{true}) = 1
 LinearAlgebra.norm(b::BasisElement) = √norm_sqr(b)
-
+transform(::PolynomialEnsemble, x) = x
 
 struct Kernel{P <: PolynomialEnsemble, T}
     ensemble::P
@@ -39,11 +40,13 @@ struct Kernel{P <: PolynomialEnsemble, T}
 end
 
 function ((; ensemble, n)::Kernel)(x, y)
+    x′ = transform(ensemble, x)
     if x == y
-        Δ = derivative(ensemble[n], x) * ensemble[n - 1](x) - derivative(ensemble[n - 1], x) * ensemble[n](x)
+        Δ = derivative(ensemble[n], x′) * ensemble[n - 1](x′) - derivative(ensemble[n - 1], x′) * ensemble[n](x′)
         Δ *= weight(ensemble, x)
     else
-        Δ = (ensemble[n](x) * ensemble[n - 1](y) - ensemble[n - 1](x) * ensemble[n](y)) / (x - y)
+        y′ = transform(ensemble, y)
+        Δ = (ensemble[n](x′) * ensemble[n - 1](y′) - ensemble[n - 1](x′) * ensemble[n](y′)) / (x′ - y′)
         Δ *= √(weight(ensemble, x) * weight(ensemble, y))
     end
     return fraction_leading_coefficients(ensemble, n) * Δ / norm_sqr(ensemble[n - 1])
@@ -70,7 +73,7 @@ function ((; ensemble, n)::BasisElement{false, <:Meixner})(x)
     (; K, q) = ensemble
     T = float(promote_type(typeof(K), typeof(q), typeof(n), typeof(x)))
     K, q, n, x = _Arb(K), _Arb(q), _Arb(n), _Arb(x)
-    return T(hypgeom_rising(x + K, n) * hypgeom_2f1(-n, -x, 1 - K - n - x, inv(q)))
+    return T((-1)^n * hypgeom_rising(x + K, n) * hypgeom_2f1(-n, -x, 1 - K - n - x, inv(q)))
 end
 function LinearAlgebra.norm_sqr((; ensemble, n)::BasisElement{false, <:Meixner})
     (; K, q) = ensemble
@@ -79,7 +82,7 @@ function LinearAlgebra.norm_sqr((; ensemble, n)::BasisElement{false, <:Meixner})
     return T(Arblib.gamma!(Arb(), n + 1) * hypgeom_rising(K, n) / ((1 - q)^K * q^n))
 end
 weight((; K, q)::Meixner, x) = binomial(x + K - 1, x) * q^x
-fraction_leading_coefficients((; q)::Meixner, _) = -q / (1 - q)
+fraction_leading_coefficients((; q)::Meixner, _) = q / (1 - q)
 
 
 @kwdef struct Krawtchouk{S, T} <: DiscretePolynomialEnsemble
@@ -127,7 +130,7 @@ function ((; ensemble, n)::BasisElement{false, <:DiscreteLegendre})(x)
     (; N) = ensemble
     T = float(promote_type(typeof(N), typeof(n), typeof(x)))
     N, n, x = _Arb(N), _Arb(n), _Arb(x)
-    return T(hypgeom_3f2(-n, 1 + n, -x, Arb(1), -N, Arb(1)))
+    return T((-1)^n * hypgeom_3f2(-n, 1 + n, -x, Arb(1), -N, Arb(1)))
 end
 function LinearAlgebra.norm_sqr((; ensemble, n)::BasisElement{false, <:DiscreteLegendre})
     (; N) = ensemble
@@ -136,7 +139,7 @@ function LinearAlgebra.norm_sqr((; ensemble, n)::BasisElement{false, <:DiscreteL
     return T(hypgeom_rising(N + 1, n + 1) / ((2n + 1) * hypgeom_rising(N - n + 1, n)))
 end
 weight((; N)::DiscreteLegendre, x) = 0 ≤ x ≤ N
-fraction_leading_coefficients((; N)::DiscreteLegendre, n) = (n * (n - N - 1)) / (4n - 2)
+fraction_leading_coefficients((; N)::DiscreteLegendre, n) = (n * (N + 1 - n)) / (4n - 2)
 
 
 @kwdef struct Hahn{S, T} <: DiscretePolynomialEnsemble
@@ -149,7 +152,7 @@ function ((; ensemble, n)::BasisElement{false, <:Hahn})(x)
     (; α, β, M) = ensemble
     T = float(promote_type(typeof(α), typeof(β), typeof(M), typeof(n), typeof(x)))
     α, β, M, n, x = _Arb(α), _Arb(β), _Arb(M), _Arb(n), _Arb(x)
-    return T(hypgeom_3f2(-n, -x, n + α + β + 1, -M, α + 1, Arb(1)))
+    return T((-1)^n * hypgeom_3f2(-n, -x, n + α + β + 1, -M, α + 1, Arb(1)))
 end
 function LinearAlgebra.norm_sqr((; ensemble, n)::BasisElement{false, <:Hahn})
     (; α, β, M) = ensemble
@@ -175,7 +178,7 @@ end
 function fraction_leading_coefficients((; α, β, M)::Hahn, n)
     T = float(promote_type(typeof(α), typeof(β), typeof(M), typeof(n)))
     α, β, M, n = Arb(α), Arb(β), Arb(M), Arb(n)
-    return T((n - M - 1) * (α + n) * hypgeom_rising(α + β + n, n - 1) / hypgeom_rising(α + β + n + 1, n))
+    return T((M + 1 - n) * (α + n) * hypgeom_rising(α + β + n, n - 1) / hypgeom_rising(α + β + n + 1, n))
 end
 
 
@@ -197,5 +200,8 @@ end
 LinearAlgebra.norm_sqr((; ensemble, n)::BasisElement{false, BesselJ{T}}) where {T} = one(T)
 weight(::BesselJ{T}, x::S) where {T, S} = one(promote_type(T, S))
 fraction_leading_coefficients((; θ)::BesselJ, n) = √θ
+
+include("lanczos.jl")
+include("lanczos_monic.jl")
 
 end
---
+++ src/lanczos.jl
@@ -0,0 +1,67 @@
+function lanczos(w, domain; ν = identity, simplify = identity, N = length(domain))
+    v₁ = fill(inv(√sum(w, domain)), length(domain))
+    v₂ = similar(v₁)
+
+    α = similar(v₁, N)
+    β = similar(v₁, N - 1)
+
+    dot(u, v) = sum(i -> w(domain[i]) * u[i] * v[i], eachindex(domain))
+
+    u = ν.(domain) .* v₁
+    α[1] = simplify(dot(u, v₁))
+    u .-= α[1] .* v₁
+
+    for j in 2:N
+        β[j - 1] = simplify(√dot(u, u))
+        v₂ .= simplify.(u ./ β[j - 1])
+        u .= ν.(domain) .* v₂ .- β[j - 1] .* v₁
+        α[j] = simplify(dot(u, v₂))
+        u .-= α[j] .* v₂
+        v₁, v₂ = v₂, v₁
+    end
+
+    return α, β
+end
+
+function clenshaw(c, x, α, β; k_max = length(α) - 1)
+    T = float(promote_type(typeof(x), eltype(α), eltype(β)))
+    N = length(α)
+    N == 0 && return zero(T)
+
+    b₂ = zero(T)
+    b₁ = zero(T)
+
+    for k in k_max:-1:0
+        b₀ = T(c(k))
+        if !iszero(b₁)
+            b₀ += (x - α[k + 1]) / β[k + 1] * b₁
+        end
+        if !iszero(b₂)
+            b₀ -= β[k + 1] / β[k + 2] * b₂
+        end
+        b₁, b₂ = b₀, b₁
+    end
+
+    return b₁
+end
+
+@kwdef struct Lanczos{T, S, F, G} <: DiscretePolynomialEnsemble
+    α::Vector{T}
+    β::Vector{T}
+    w::F
+    sum_w::S
+    ν::G = identity
+end
+function Lanczos(w, domain; ν = identity, simplify = identity)
+    α, β = lanczos(w, domain; ν, simplify)
+    return Lanczos(; α, β, w, sum_w = sum(w, domain), ν)
+end
+
+function ((; ensemble, n)::BasisElement{false, <:Lanczos{T}})(x) where {T}
+    (; α, β) = ensemble
+    return clenshaw(==(n), x, α, β; k_max = Int(n))
+end
+LinearAlgebra.norm_sqr((; ensemble)::BasisElement{false, <:Lanczos}) = ensemble.sum_w
+weight((; w)::Lanczos, x) = w(x)
+fraction_leading_coefficients((; β)::Lanczos, n) = β[Int(n)]
+transform((; ν)::Lanczos, x) = ν(x)
